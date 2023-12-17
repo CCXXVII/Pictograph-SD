7a58961a238fbfd597f0a0dc6a8075eb824984e7
Enes Dertli <enesdertli05@hotmail.com>
2023-12-17 15:42:58 +0300

    added options

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import interactions
 from interactions import Client, Intents, listen, slash_command, SlashContext, slash_option, OptionType
 import os
 import dotenv
-import json
 import requests
 import io
 import base64
+from interactions import SlashCommandChoice
 
 bot = Client(intents=Intents.DEFAULT)
 
@@ -20,29 +20,111 @@ async def on_message_create(event):
 
 #* COMMANDS
 #* FIRST COMMAND
-
 url = "http://127.0.0.1:7860"
 @slash_command(name="image", description="Creates images with Stability Diffusion")
+
+
+#! FIRST OPTION OF IMAGE COMMAND
 @slash_option(
     name="prompt",
-    description="The prompt ",
+    description="Enter a text prompt to generate an image. Describe the scene or concept you have in mind.",
     required=True,
     opt_type=OptionType.STRING
 )
-async def image_function(ctx: SlashContext, prompt : str):
+
+#! SECOND OPTION OF IMAGE COMMAND
+@slash_option(
+    name="style",
+    description="Choose a style for the image.",
+    required=False,
+    opt_type=OptionType.STRING,
+    choices=[
+        SlashCommandChoice(name="digital art", value="digital art"),
+        SlashCommandChoice(name="anime", value="anime"),
+        SlashCommandChoice(name="cinematic", value="cinematic"),
+        SlashCommandChoice(name="analog film", value="analog film"),
+        SlashCommandChoice(name="digital painting", value="digital painting"),
+        SlashCommandChoice(name="black and white", value="black and white"),
+        SlashCommandChoice(name="fantasy art", value="fantasy art"),
+        SlashCommandChoice(name="cyber punk", value="cyber punk"),
+        SlashCommandChoice(name="vector illustrations", value="vector illustration"),
+        SlashCommandChoice(name="realistic photo portraits", value="realistic photo portraits"),
+    ]
+)
+
+#! THIRD OPTION OF IMAGE COMMAND
+@slash_option(
+    name="negativeprompt",
+    description="An optional text that provides a contrasting or opposing idea to the main prompt.",
+    required=False,
+    opt_type=OptionType.STRING
+)
+
+#! FOURTH OPTION OF IMAGE COMMAND
+@slash_option(
+    name="steps",
+    description="",
+    required=False,
+    opt_type=OptionType.INTEGER,
+    min_value=10,
+    max_value=50
+)
+#! FIFTH OPTION OF IMAGE COMMAND
+@slash_option(
+    name="width",
+    description="",
+    required=False,
+    opt_type=OptionType.INTEGER,
+    min_value=312,
+    max_value=1024
+)
+
+#! SIXTH OPTION OF IMAGE COMMAND
+@slash_option(
+    name="height",
+    description="",
+    required=False,
+    opt_type=OptionType.INTEGER,
+    min_value=312,
+    max_value=1024
+)
+
+
+#* RUNNING FUNCTION
+async def image_function(ctx: SlashContext, 
+                         prompt: str, 
+                         style: str = "Digital Art", 
+                         negative_prompt: str="", 
+                         width: int = 512, 
+                         height: int = 512,
+                         steps: int = 20
+                         
+                         ):
+    
+    
+
     payload = {
         "prompt": prompt,
-        "steps": 5
+        "negative_prompt" : negative_prompt,
+        "styles": [style],
+        "steps": steps,
+        "width" : width,
+        "height" : height
     }
 
     response = requests.post(url=f"{url}/sdapi/v1/txt2img", json=payload)
     r = response.json()
-
     image_bytes = base64.b64decode(r['images'][0])
-    #await ctx.send(file=interactions.File(io.BytesIO(image_bytes), file_name="image.png"), content={prompt})
+    
+
+    # A display method to to see the image in jupyter > 
+    # >>>>>  display(Image(data=img.getvalue(), format='png'))
     mention = f"<@{ctx.author.id}>"
     await ctx.send(f"**Prompt: {prompt}** by {mention}  ", file=interactions.File(io.BytesIO(image_bytes), file_name="image.png"))
-    
+
+
+
+
 
 
 
